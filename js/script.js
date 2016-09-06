c794a9b4b97ce7157555cad84bab273ce892173b
cao <caominhtung64@gmail.com>
2016-09-06 14:37:32 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$('.caret').click(function() {
-	$('.user-logout').toggle();
+jQuery( document ).ready(function($){
+	$('.caret').click(function() {
+		$('.user-logout').toggle();
+	});
+	$('.alphabet-option').click(function(event) {
+		event.preventDefault();
+		var ok = $(this).data('value');
+		//alert(ok);
+		$('.box-alphabet').val($(this).data('value'));
+		//return false;
+	});
 });
+
